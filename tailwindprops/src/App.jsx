1ac4ceabe94d4195576ed60c500daf9dac961932
Amansingh-gif <amansinghasn830@gmail.com>
2025-07-24 16:28:11 +0530

    understanding of props

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+   
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4' >tailwind test</h1>
+    <Card  username="hitesh"/>
+    <Card  username="badmas"/>
+    <Card  username="sadmash"/>
+
+  </>
+
+    
+  )
+}
+
+export default App
